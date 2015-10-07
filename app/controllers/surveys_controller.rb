884ec6c3fd295cf4e29453630d9a2a058fb75ae2
Luis Mario Urrea Murillo <luis.urrea@campusucc.edu.co>
2015-10-07 18:24:06 -0500

    integrated design on surveys and questions views

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -10,6 +10,7 @@ class SurveysController < ApplicationController
   # GET /surveys/1
   # GET /surveys/1.json
   def show
+
   end
 
   # GET /surveys/new
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   self.inheritance_column = nil
   TYPES = ["Dichotomous", "Nominal", "Ordinal", "Interval", "Semantic differential"]
+
 end
